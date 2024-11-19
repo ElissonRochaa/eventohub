cd3b89c4a58f98f0eae1d87bb69c677dcf5a41a7
Elisson Rocha <elissonrochaa@gmail.com>
2024-11-19 17:40:17 -0300

    ajuste banco

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://usuario:senha@ip:porta/nome_do_banco')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123@localhost:5432/eventohub_fap')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres.ybckbvogxccgupsxgbsr:Ev3nt0hub123v3nt0@aws-0-us-west-1.pooler.supabase.com:6543/postgres')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
