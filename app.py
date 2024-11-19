c43c97d867f8801231cb1aeb7b59e7451ca9df79
Elisson Rocha <elissonrochaa@gmail.com>
2024-11-19 18:42:42 -0300

    ajuste app 2

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ app.register_blueprint(inscricao_bp, url_prefix='/api/inscricoes')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
